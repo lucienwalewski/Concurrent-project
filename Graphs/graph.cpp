e4a0da5167c5a3d373bb452b3c74e2d7419df623
lucienwalewski <lucien.walewski@polytechnique.edu>
2022-05-22 14:31:05 +0200

    Added some extra functionality to the graphs

--- Graphs/graph.cpp
+++ Graphs/graph.cpp
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <set>
 
 using namespace std;
 
@@ -20,17 +21,59 @@ struct Edge {
 // The state of the graph is stored in an adjacency matrix
 class Graph {
     public:
-        int n; // number of vertices
+        int n; // size of graph
         vector<vector<int> > adjMatrix; // adjacency matrix
+        set<pair<int, int> > edges; // edges of the graph
 
         // Constructor
         Graph(int n) {
             this->n = n;
             this->adjMatrix = vector<vector<int> >(n, vector<int>(n, 0));
         }
+
+        // Constructor with adjency matrix
+        Graph(int n, vector<vector<int> > adjMatrix) {
+            this->n = n;
+            this->adjMatrix = adjMatrix;
+            // Initialize edges
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (adjMatrix[i][j] != 0) {
+                        this->edges.insert(make_pair(i, j));
+                    }
+                }
+            }
+        }
+        
+        // Display the graph
+        void display() {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    cout << adjMatrix[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+
+
+        // Function to find successors of a vertex
+        set<int> getSuccessors(int u);
 };
 
+set<int> Graph::getSuccessors(int u) {
+    set <int> successors;
+    for (int v = 0; v < n; v++) {
+        if (adjMatrix[u][v] != 0) {
+            successors.insert(v);
+        }
+    }
+    return successors;
+}
+
 int main() {
-    Graph g(5);
+    // Create a simple graph
+    vector<vector<int> > graph(4, vector<int>(4, 0));
+    Graph g(4, graph);
+    g.display();
 }
 
